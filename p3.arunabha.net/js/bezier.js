c4d51bae0ac4899f447d5d968990a143a46e4e14
Arunabha nag <nag.arunabha@gmail.com>
2012-11-24 23:31:36 -0500

    Final changes

--- p3.arunabha.net/js/bezier.js
+++ p3.arunabha.net/js/bezier.js
@@ -1,44 +1,57 @@
 
 $(document).ready(function() { // start doc ready; do not delete this!
 
+	//Create a canvas element
 	var canv = document.createElement('canvas');
+	
+	//Check if browser supports html5
 	if (!canv.getContext) 
 	{
-	   alert("Your browser does not support html 5. Please use Chrome, IE 9 or later.");
+	   alert("Your browser does not support html 5. Please use Chrome, IE ver. 9 or later.");
 	   return;
 	}
 
+	//Set canvas size
 	canv.width = 600;
 	canv.height = 400;
+	
+	//Add canvas to the div with id "view"
 	$("#view").get(0).appendChild(canv);
 
+	//Create a CanvasState object.
 	var cState = new CanvasState($('canvas').get(0));
 
+	//Handle mousedown event
 	$("canvas").mousedown(function(e) {
-
+		//Find out local coordinates
 		var x = e.clientX - this.offsetLeft;
 		var y = e.clientY - this.offsetTop;
 		cState.mousedown(x, y);
 	});
 
+	//Handle mousemove event
 	$("canvas").mousemove(function(e) {
-
+		//Find out local coordinates
 		var x = e.clientX - this.offsetLeft;
 		var y = e.clientY - this.offsetTop;
 		cState.mousemove(x, y);
 	});
 
+	//Handle mouseup event
 	$("canvas").mouseup(function(e) 
 	{
+		//Find out local coordinates
 		var x = e.clientX - this.offsetLeft;
 		var y = e.clientY - this.offsetTop;
 		cState.mouseup(x, y);
 	});
 
+	//Clear canvas
 	$("#clearCanvas").click(function() {
 	  cState.clear();
 	});
 
+	//Remove last control point
 	$("#deleteLast").click(function() {
 
 		cState.ctrlPoints.pop();
@@ -52,6 +65,7 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		this.rad = 5;
 	}
 
+	//Returns true if (x, y) is within radius of 5 pixels
 	Point.prototype.isSamePoint = function(x, y)
 	{
 		var dx = this.X - x;
@@ -59,18 +73,23 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		return (dx * dx + dy * dy) <= 25;
 	}
 
+	//Draws a point.
 	Point.prototype.draw = function(ctx)
 	{
+		//Draw a filled circle
 		ctx.beginPath();
 		ctx.arc(this.X, this.Y, this.rad, 0, 2 * Math.PI, true);
 		ctx.closePath(); // Close the path
 		ctx.fillStyle = 'red';
 		ctx.fill();
+		
+		//Draw a line circle
 		ctx.lineWidth = 1;
 		ctx.strokeStyle = 'black';
 		ctx.stroke();
 	}
 
+	//Initialize CanvasState
 	function CanvasState(cnv)
 	{
 		this.canvas = cnv;
@@ -80,17 +99,20 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		this.dragStartY = 0;
 	}
 
+	//Adds a control point to canvas
 	CanvasState.prototype.addCtrlPoint = function(point)
 	{
 		this.ctrlPoints.push(point);
 	}
 
+	//Removes all the control points
 	CanvasState.prototype.clear = function()
 	{
 		this.ctrlPoints = [];
 		this.draw();
 	}
 
+	//Draws canvas
 	CanvasState.prototype.draw = function()
 	{
 		var ctx = this.canvas.getContext("2d");
@@ -130,6 +152,9 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		//Draw the Bezier curve
 		if (len < 2)
 			return;
+			
+		//Draw Bezier curve between parameter range [0 , 1]
+		//Draw approximate lines in 50 steps that follows the curve
 		var t = 0.0;
 		var nSteps = 50;
 		var delta = 1.0 / nSteps;
@@ -138,7 +163,6 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		ctx.strokeStyle = 'red';
 		while(t <= 1.0)
 		{
-			//console.log(t);
 			var cp = this.pointOnCurve(t);
 			if (t < delta)
 			{
@@ -148,6 +172,7 @@ $(document).ready(function() { // start doc ready; do not delete this!
 			else
 				ctx.lineTo(cp.X, cp.Y);
 			
+			// t cannot be more than 1.0. 
 			if (t < 1.0 && t+delta > 1.0)
 				t = 1.0;
 			else
@@ -156,6 +181,7 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		ctx.stroke();
 	}
 
+	//returns the point on Bezier curve at parameter 't'
 	CanvasState.prototype.pointOnCurve = function(t)
 	{
 		var bernsteins = [];
@@ -192,6 +218,7 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		return point;
 	}
 
+	//Returns the Control point at a given x, y position (if exists)
 	CanvasState.prototype.pointUnder = function(x, y)
 	{
 		var len = this.ctrlPoints.length;
@@ -207,6 +234,7 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		return null;
 	}
 
+	//Set the selected point
 	CanvasState.prototype.mousedown = function(x, y)
 	{
 		this.selected = this.pointUnder(x, y);
@@ -214,6 +242,7 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		this.dragStartY = y;
 	}
 
+	//Drag the select control point
 	CanvasState.prototype.mousemove = function(x, y)
 	{
 		if (this.selected != null)
@@ -228,11 +257,9 @@ $(document).ready(function() { // start doc ready; do not delete this!
 			this.selected.Y += deltaY;
 			this.draw();
 		}
-		else if (this.pointUnder(x, y) != null)
-		{
-		}
 	}
 
+	//Deselect the point on mouseup. If nothing was selected, add a control point at the pick point.
 	CanvasState.prototype.mouseup = function(x, y)
 	{
 		if (this.selected == null)
@@ -245,6 +272,7 @@ $(document).ready(function() { // start doc ready; do not delete this!
 		}
 	}
 
+	//Add a control point.
 	function addControlPoint(cnv, x, y)
 	{
 		var cp = new Point(x, y);
